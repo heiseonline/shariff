21305be9a6d4b1f1e7aa2f3330b6768e2751eceb
mikejpr <michael_rapp@gmx.net>
2015-03-05 22:01:11 +0100

    Add fr,it,da,nl

--- src/js/services/mail.js
+++ src/js/services/mail.js
@@ -16,7 +16,11 @@ module.exports = function(shariff) {
         title: {
             'de': 'Per E-Mail versenden',
             'en': 'Send by email',
-            'es': 'Enviar por email'
+            'es': 'Enviar por email',
+            'fr': 'Envoyer par courriel',
+            'it': 'Inviare via email',
+            'da': 'Sende via e-mail',
+            'nl': 'Sturen via e-mail '
         },
         shareUrl: url.format(shareUrl)
     };
