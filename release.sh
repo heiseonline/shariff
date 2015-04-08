5d63c6ce8408590c8c9d03be328e5e90f8fe186b
pmb <pmb@heise.de>
2015-04-08 12:04:42 +0200

    bower needs build folder

--- release.sh
+++ release.sh
@@ -6,12 +6,13 @@ VVERSION=v$VERSION
 ZIP=shariff-$VERSION.zip
 
 SVERSION=$VERSION perl -pi -E 's{^(\s+"version":\s*)"[\d.]+"}{${1}"$ENV{SVERSION}"}' package.json bower.json
-git add bower.json package.json
+grunt build
+git add bower.json package.json build
 git commit -m "release $VVERSION"
 git tag $VVERSION
-grunt build
 7z a -tzip $ZIP ./build/*
-rm -fr build
+# rm -fr build
 github-release release -t $VVERSION -n $VERSION
 github-release upload -t $VVERSION -n $ZIP -f $ZIP
 rm $ZIP
+npm publish
