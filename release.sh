13d92dced309b9cbea0edfe6367217f261af9a01
apl <apl@heise.de>
2015-09-11 12:23:49 +0200

    fix github-release not pointing to the release commit

--- release.sh
+++ release.sh
@@ -9,11 +9,13 @@ SVERSION=$VERSION perl -pi -E 's{^(\s+"version":\s*)"[\d.]+"}{${1}"$ENV{SVERSION
 grunt build
 git add bower.json package.json build
 git commit -m "release $VVERSION"
+git push
 # git tag $VVERSION
 7z a -tzip $ZIP ./build/*
 # rm -fr build
-github-release release -t $VVERSION -n $VERSION
-github-release upload -t $VVERSION -n $ZIP -f $ZIP
-git push --follow-tags
+github-release release -u heiseonline -r shariff -t $VVERSION -n $VERSION
+github-release upload -u heiseonline -r shariff -t $VVERSION -n $ZIP -f $ZIP
+git push
+git push --tags
 rm $ZIP
 npm publish
