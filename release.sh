1daf6978481879ccfbf5971564559b87912b1d68
Alexander Pluhar <apl@heise.de>
2016-06-29 10:08:22 +0200

    abort release if version is missing

--- release.sh
+++ release.sh
@@ -5,12 +5,18 @@ VERSION=$1
 VVERSION=v$VERSION
 ZIP=shariff-$VERSION.zip
 
+if [ "$1" == "" ]
+    then
+        echo "Please provide version information."
+        exit 1
+fi
+
 SVERSION=$VERSION perl -pi -E 's{^(\s+"version":\s*)"[\d.]+"}{${1}"$ENV{SVERSION}"}' package.json bower.json
 grunt build
 git add bower.json package.json build
 git commit -m "release $VVERSION"
 git push
-# git tag $VVERSION
+git tag $VVERSION
 7z a -tzip $ZIP ./build/*
 # rm -fr build
 # https://github.com/aktau/github-release
