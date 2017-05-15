85ec590ed749002eb7baaa8cee26864e6c8b70f1
glaszig <glaszig@gmail.com>
2017-05-15 18:05:24 +0200

    added Object.assign polyfill for older browsers (ie11) -- #220

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,5 +1,31 @@
 'use strict';
 
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+if (typeof Object.assign !== 'function') {
+  // jshint maxdepth:4
+  Object.assign = function(target, varArgs) { // .length of function is 2
+    if (target === null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource !== null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
+
 /**
  * Initialization helper. This method is this module's exported entry point.
  * @param {string|Array|Element} selector - css selector, one element, array of nodes or html fragment
