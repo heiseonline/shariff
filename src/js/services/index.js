8f871dfb72fe40095b3f8c1c0164820f3eaaac70
Dennis Stumm <dstumm@yandex.ru>
2018-01-02 08:08:46 +0100

    Add vk service

--- src/js/services/index.js
+++ src/js/services/index.js
@@ -17,6 +17,7 @@ module.exports = {
   threema: require('./threema'),
   tumblr: require('./tumblr'),
   twitter: require('./twitter'),
+  vk: require('./vk'),
   weibo: require('./weibo'),
   whatsapp: require('./whatsapp'),
   xing: require('./xing')
---
+++ src/js/services/vk.js
@@ -0,0 +1,63 @@
+'use strict'
+
+module.exports = function(shariff) {
+  var url = encodeURIComponent(shariff.getURL())
+  return {
+    popup: true,
+    shareText: {
+      'bg': 'cподеляне',
+      'da': 'del',
+      'de': 'teilen',
+      'en': 'share',
+      'es': 'compartir',
+      'fi': 'Jaa',
+      'fr': 'partager',
+      'hr': 'podijelite',
+      'hu': 'megosztás',
+      'it': 'condividi',
+      'ja': '共有',
+      'ko': '공유하기',
+      'nl': 'delen',
+      'no': 'del',
+      'pl': 'udostępnij',
+      'pt': 'compartilhar',
+      'ro': 'partajează',
+      'ru': 'поделиться',
+      'sk': 'zdieľať',
+      'sl': 'deli',
+      'sr': 'podeli',
+      'sv': 'dela',
+      'tr': 'paylaş',
+      'zh': '分享'
+    },
+    name: 'vk',
+    faName: 'fa-vk',
+    title: {
+      'bg': 'Сподели във VK',
+      'da': 'Del på VK',
+      'de': 'Bei VK teilen',
+      'en': 'Share on VK',
+      'es': 'Compartir en VK',
+      'fi': 'Jaa VKissa',
+      'fr': 'Partager sur VK',
+      'hr': 'Podijelite na VKu',
+      'hu': 'Megosztás VKon',
+      'it': 'Condividi su VK',
+      'ja': 'フェイスブック上で共有',
+      'ko': '페이스북에서 공유하기',
+      'nl': 'Delen op VK',
+      'no': 'Del på VK',
+      'pl': 'Udostępnij na VKu',
+      'pt': 'Compartilhar no VK',
+      'ro': 'Partajează pe VK',
+      'ru': 'Поделиться на ВКонтакте',
+      'sk': 'Zdieľať na VKu',
+      'sl': 'Deli na VKu',
+      'sr': 'Podeli na VK-u',
+      'sv': 'Dela på VK',
+      'tr': 'VK\'ta paylaş',
+      'zh': '在VK上分享',
+    },
+    shareUrl: 'https://vk.com/share.php?url=' + url + shariff.getReferrerTrack()
+  }
+}
