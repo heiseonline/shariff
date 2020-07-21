83b2301ab6cc1b34fd12a42cfccff3e175870333
No Name Pro <noname.pro.nn@gmail.com>
2020-07-21 13:16:20 +0200

    Update LinkedIn Link to 2020 Version

--- src/js/services/linkedin.js
+++ src/js/services/linkedin.js
@@ -2,8 +2,6 @@
 
 module.exports = function(shariff) {
   var url = encodeURIComponent(shariff.getURL())
-  var title = encodeURIComponent(shariff.getTitle())
-  var descr = encodeURIComponent(shariff.getMeta('description'))
   return {
     popup: true,
     shareText: {
@@ -63,6 +61,6 @@ module.exports = function(shariff) {
       'tr': 'LinkedIn\'ta paylaş',
       'zh': '在LinkedIn上分享'
     },
-    shareUrl: 'https://www.linkedin.com/shareArticle?mini=true&summary=' + descr + '&title=' + title + '&url=' + url
+    shareUrl: 'https://www.linkedin.com/sharing/share-offsite/?url=' + url
   }
 }
