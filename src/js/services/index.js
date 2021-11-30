ef5e15162160a3687683c62bbe3f5a13698a80fe
Christoph Jüngling <chris@juengling-edv.de>
2021-11-30 13:55:54 +0100

    Add button for Mastodon/Fediverse (#370)
    Maybe it's not yet sufficient, but perhaps it's a start :-)

--- src/js/services/index.js
+++ src/js/services/index.js
@@ -3,6 +3,7 @@ module.exports = {
   buffer: require('./buffer'),
   diaspora: require('./diaspora'),
   facebook: require('./facebook'),
+  fediverse: require('./fediverse'),
   flattr: require('./flattr'),
   flipboard: require('./flipboard'),
   info: require('./info'),
